40af05ce39c9c6a60476683be79eb411fd68a5f0
vkset1 <yicheng.peng@siemens.com>
2018-12-28 17:04:00 +0800

    typescript playground

--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,10 @@
 import { sayHello } from "./greet";
 
+/**
+ * Import a module
+ * @param divName 
+ * @param name 
+ */
 function showHello(divName: string, name: string) {
     const elt = document.getElementById(divName);
     elt.innerText = sayHello(name);
@@ -12,6 +17,10 @@ function test() {
 showHello("greeting", "TypeScript");
 test();
 
+
+/**
+ * Class Inheritance
+ */
 class BankAccount {
     constructor(public balance: number) {
     }
@@ -23,7 +32,6 @@ class BankAccount {
         return this.balance;
     }
 }
-
 class CheckingAccount extends BankAccount {
     constructor(balance: number) {
         super(balance);
@@ -33,8 +41,15 @@ class CheckingAccount extends BankAccount {
         return this.balance;
     }
 }
+var account = new CheckingAccount(100);
+console.log(account.getBalance());
+console.log(account.deposit(100))
+console.log(account.writeCheck(50));
 
-const enum Operator {
+/**
+ * Types Enum
+ */
+enum Operator {
     ADD,
     DIV,
     MUL,
@@ -42,7 +57,7 @@ const enum Operator {
 };
 
 function compute(op: Operator, a: number, b: number) {
-    // console.log("the operator is " + Operator[op]);
+    console.log("the operator is " + Operator[op]);
     switch(op) {
         case Operator.ADD:
             console.log("the result is " + (a + b));
@@ -55,8 +70,71 @@ function compute(op: Operator, a: number, b: number) {
     }
 }
 
-compute(0, 1, 2);
-var account = new CheckingAccount(100);
-console.log(account.getBalance());
-console.log(account.deposit(100))
-console.log(account.writeCheck(50));
+compute(Operator.ADD, 1, 2);
+
+/**
+ * Generic Types and Functions
+ */
+interface Array<T> {
+    reverse(): T[];
+    sort(compareFn?: (a: T, b: T) => number): T[];
+    //  ... 
+}
+function numberToString(a: number[]) {
+    var stringArray = a.map(v => v.toString());
+    return stringArray;
+}
+
+var aList = [1, 2, 3];
+var aListStr = numberToString(aList);
+document.writeln(aListStr.toString());
+
+/**
+ * Namespace
+ */
+namespace M {
+    var s = "hello";
+    export function f() {
+        return s;
+    }
+}
+console.log(M.f());
+
+/**
+ * Mixins
+ */
+class a {
+    load() {
+        console.log('a: load');
+    }
+    loadProductStructure() {
+        console.log('a: load product structure')
+    }
+}
+class b {
+    load() {
+        console.log('b: load');
+    }
+    loadProductStructure() {
+        console.log('b: load product structure')
+    }
+}
+
+class SmartObject implements a, b { 
+    load: () => void;
+    loadProductStructure: () => void;
+}
+
+applyMixins(SmartObject, [a, b]);
+
+function applyMixins(derivedCtor: any, baseCtors: any[]) {
+    baseCtors.forEach(baseCtor => {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+            derivedCtor.prototype[name] = baseCtor.prototype[name];
+        });
+    });
+}
+
+let sObj = new SmartObject();
+sObj.load();
+sObj.loadProductStructure();
