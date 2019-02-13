9f953d8388571e49c209bb109243f8acb2291d52
vkset1 <yicheng.peng@siemens.com>
2019-02-13 10:56:38 +0800

    demo2 material rendering order

--- js/PMI.js
+++ js/PMI.js
@@ -13,6 +13,28 @@ Node.prototype = {
     }
 }
 
+var PmiObject = function ( name ) {
+    THREE.Object3D.call( this );
+    this.name = name;
+    this.type = "PmiObject";
+};
+
+PmiObject.prototype = Object.create( THREE.Object3D.prototype );
+PmiObject.prototype.constructor = PmiObject;
+
+Object.defineProperties( PmiObject.prototype, {
+    isLoaded: {
+        enumerable: true,
+        writable: true,
+        value: false
+    },
+    isLoading: {
+        enumerable: true,
+        writable: true,
+        value: false
+    }
+});
+
 var PmiNode = function() {};
 Node.prototype.apply(PmiNode.prototype);
 PmiNode.prototype.constructor = PmiNode;
@@ -39,10 +61,48 @@ PmiText.prototype = Object.create( PmiLine.prototype );
 PmiText.prototype.constructor = PmiText;
 PmiNode.prototype.apply( PmiText.prototype );
 
+var PmiRefPlane = function ( geometry, material ) {
+    THREE.Object3D.call( this );
+    this.type = "PmiRefPlane";
+    this.geometry = geometry;
+    this._shapeMaterial = material;
+    this.matrixAutoUpdate = false;
+    this.updateMatrixWorld( true );
+    this.drawMode = THREE.TrianglesDrawMode;
+}
+
+PmiRefPlane.prototype = Object.create( THREE.Mesh.prototype );
+PmiRefPlane.prototype.constructor = PmiRefPlane;
+PmiNode.prototype.apply( PmiRefPlane.prototype );
+
+Object.defineProperties( PmiRefPlane.prototype, {
+    material: {
+        enumerable: true,
+        get: function() {
+            return this._shapeMaterial;
+        },
+        set: function( value ) {
+            this._shapeMaterial = value;
+        }
+    }
+});
+
+PmiRefPlane.prototype.clone = function ( object, recursive ) {
+    if (!object) {
+        object = new PmiRefPlane( this.geometry, this._shapeMaterial );
+    }
+
+    THREE.Object3D.prototype.clone.call( this, object, recursive );
+
+    return object;
+}
+
 var PMIEntity = {
+    PmiObject,
     PmiLine,
     PmiNode,
-    PmiText
+    PmiText,
+    PmiRefPlane
 };
 
 export default PMIEntity;
